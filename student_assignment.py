aadddeb3d3bca83623bde766cf2d819283d30c39
ChunHsiang Chang <chunhsiang_chang@asus.com>
2025-03-04 18:33:10 +0800

    Add Answer Q1

--- student_assignment.py
+++ student_assignment.py
@@ -1,10 +1,15 @@
 import datetime
+import time
+
 import chromadb
 import traceback
+import csv
 
 from chromadb.utils import embedding_functions
 
 from model_configurations import get_model_configuration
+
+from langchain_chroma import Chroma
 
 gpt_emb_version = 'text-embedding-ada-002'
 gpt_emb_config = get_model_configuration(gpt_emb_version)
@@ -12,7 +17,29 @@ gpt_emb_config = get_model_configuration(gpt_emb_version)
 dbpath = "./"
 
 def generate_hw01():
-    pass
+    chroma_client = chromadb.PersistentClient(path=dbpath)
+    collection = chroma_client.get_or_create_collection(
+        name="TRAVEL",
+        metadata={"hnsw:space": "cosine"}
+    )
+    with open('COA_OpenData.csv', encoding="utf-8-sig") as csvfile:
+        rows = csv.DictReader(csvfile)
+        for row in rows:
+            crateDateTimeString = str.strip(row["CreateDate"])
+            crateDateTime = datetime.datetime.strptime(crateDateTimeString,"%Y-%m-%d")
+            crateDateTimeStamp = int(time.mktime(crateDateTime.timetuple()))
+            collection.add(
+                ids=[row["ID"]],
+                documents=[row["HostWords"]],
+                metadatas=[{"file_name": "COA_OpenData.csv",
+                            "name": row["Name"],
+                            "type":row["Type"],
+                            "address":row["Address"],
+                            "tel":row["Tel"],
+                            "city":row["City"],
+                            "town":row["Town"],
+                            "date":crateDateTimeStamp}])
+    return collection
     
 def generate_hw02(question, city, store_type, start_date, end_date):
     pass
@@ -34,5 +61,7 @@ def demo(question):
         metadata={"hnsw:space": "cosine"},
         embedding_function=openai_ef
     )
-    
     return collection
+
+# if __name__ == "__main__" :
+#     print(generate_hw01())
