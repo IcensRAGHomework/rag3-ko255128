1fc88afcffd3732382f4a75e9d661f373be484e7
ChunHsiang Chang <chunhsiang_chang@asus.com>
2025-03-05 19:57:38 +0800

    fix wrong collection count check

--- student_assignment.py
+++ student_assignment.py
@@ -46,7 +46,7 @@ def query_result_to_dictlist(query_result):
 
 def generate_hw01():
     collection = get_db_collection()
-    if collection.count() == 0:
+    if collection.count() > 0:
         return collection
     with open('COA_OpenData.csv', encoding="utf-8-sig") as csvfile:
         rows = csv.DictReader(csvfile)
